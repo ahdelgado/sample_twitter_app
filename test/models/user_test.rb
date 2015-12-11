e780843cb31fe301e76aa880b8b49062bd79ae97
AJ Ruiz <ahr@alumni.cmu.edu>
2015-12-11 22:13:04 +0000

    Chapter 6 exercises

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -42,7 +42,7 @@ class UserTest < ActiveSupport::TestCase
   
     test "email validation should reject invaild email addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+                           foo@bar_baz.com foo@bar+baz.com, foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -66,4 +66,9 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
   
+  test "email adresses should be saved as lower case" do
+    @user.email = "Foo@ExAMPle.CoM"
+    @user.save
+    assert_equal @user.email.downcase, @user.reload.email
+  end
 end
