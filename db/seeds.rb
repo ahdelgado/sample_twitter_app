73158dc1e51dee6176c42ac4cc16d22e3395feb0
AJ Ruiz <ahr@alumni.cmu.edu>
2017-05-22 04:03:19 +0000

    Modify seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,26 @@
-User.create!(name: 'AJ Ruiz',
-             email: 'ahr@alumni.cmu.edu',
+User.create!(name:  'Alcides Ruiz',
+             email: 'aj.ruiz@sv.cmu.edu',
              password:              'foobar',
              password_confirmation: 'foobar',
-             admin: true,
+             admin:     true,
              activated: true,
-             activated_at: Time.zone.now
-             )
-             
+             activated_at: Time.zone.now)
+
 99.times do |n|
-  User.create!(name: Faker::Name.name,
-             email: "example-#{n+1}@railstutorial.org",
-             password:              'password',
-             password_confirmation: 'password',
-             activated: true,
-             activated_at: Time.zone.now
-             )
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  p ['email', email]
+  password = "password"
+  User.create!(name:  name,
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
 
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content) }
+  users.each { |user| user.microposts.create(content: content) }
 end
